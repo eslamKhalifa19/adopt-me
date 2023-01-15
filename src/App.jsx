1ad6cb56312372c8eca1fb0fbfb6994bc122459c
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-15 20:07:31 +0200

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Pet from "./Pet";
 import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
