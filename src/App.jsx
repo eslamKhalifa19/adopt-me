aa98afcd835ec1d282fa7fbd0c316c03e601adb9
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-16 07:49:56 +0200

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
