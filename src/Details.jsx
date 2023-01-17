731fabb16cc74ee37a821510515c8cf8c7e9fa2c
Eslam Khalefa <eslamkhalifa842@gmail.com>
2023-01-17 17:26:31 +0200

    class_components

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousal";
+
 const Details = () => {
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
@@ -17,6 +19,7 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
